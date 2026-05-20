daf242e07c553eac24ce6f5a57bff878513b3d56
Dom Pukallus <dpukallus9@gmail.com>
2026-05-20 16:08:55 +1000

    completed challenge, to refactor the styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,16 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
